f58b28fe1747251fa6911064d7fc8ef2f290cfe2
zettat123 <zettat123@gmail.com>
2018-09-25 10:16:59 +0800

    Check login

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 
-const UploadPage = () => <div>UploadPage</div>
+class UploadPage extends React.Component {
+  //   constructor(props) {
+  //     super(props)
+  //   }
 
-export default UploadPage
+  componentWillMount() {
+    if (!sessionStorage.getItem('isLoggedIn')) {
+      this.props.history.push('/login')
+    }
+  }
+
+  render() {
+    return <div>UploadPage</div>
+  }
+}
+
+export default withRouter(UploadPage)
